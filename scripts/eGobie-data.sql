16996b30d68fc23ed7cf961fca67bbee972f9802
bhuang3 <bhuang1228@gmail.com>
2016-05-29 11:47:01 -0400

    change test data

--- scripts/eGobie-data.sql
+++ scripts/eGobie-data.sql
@@ -1711,8 +1711,8 @@ INSERT INTO car_model (id, car_maker_id, name, title) VALUES
 
 INSERT INTO user (id, type, username, password, email, phone_number, discount) VALUES
 (1, 'RESIDENTIAL', 'e', 'bc254388680ed7c7e426b417e81f41b6af7ef319', 'e@egobie.com', '2019120383', 10),
-(2, 'EGOBIE', 'egobie_em_1', '53d298bedaf32c061a7efbc58776d22c892ab02a04ff09244f6be43556f0668c', 'em1@egobie.com', '2019120383', 0),
-(3, 'EGOBIE', 'egobie_em_2', 'cd06a3a1f754044f78668d7af1901a015e9787c8514e8d86968a53e05c727404', 'em2@egobie.com', '2019120383', 0),
+(2, 'EGOBIE', 'em1', 'bc254388680ed7c7e426b417e81f41b6af7ef319', 'em1@egobie.com', '2019120383', 0),
+(3, 'EGOBIE', 'em2', 'bc254388680ed7c7e426b417e81f41b6af7ef319', 'em2@egobie.com', '2019120383', 0),
 (4, 'EGOBIE', 'a', '5df3bf93b9153cc6ab5ef8f32ad7ce3fd06ae656', 'a@egobie.com', '2019120383', 0),
 (5, 'RESIDENTIAL', 'egobie', 'b623e6fda297e4b589815902c5ec3bee0cf75891cd5fbb64', 'egobie@egobie.com', '1234567890', 0);
 
